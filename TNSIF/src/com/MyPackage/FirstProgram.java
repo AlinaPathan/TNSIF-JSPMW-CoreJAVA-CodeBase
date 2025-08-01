c6f71bb8546670f707c0756bec4acf42796f002a
HP VICTUS <HP VICTUS@DESKTOP-U3RLE1B>
2025-08-01 17:59:48 +0530

    variable in java studied

--- TNSIF/src/com/MyPackage/FirstProgram.java
+++ TNSIF/src/com/MyPackage/FirstProgram.java
@@ -5,6 +5,37 @@ public class FirstProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Hello World");
+    // TODO Auto-generated method stub
+   	//variable declaration-invalid 
+   		//int for=12;    keyword use
+   		//int num 1=34;   no space allowed 
+   		
+   		
+   		//valid identifier
+//   		int num =23;
+//   		System.out.println( "value of num"+ num);
+//   		String studentName="Alina";
+//   		System.out.println("name of student is "+studentName);
+   		
+   		
+   		
+   		int value1=9/2;
+   		System.out.println(value1);
+   		float value2=100f/3f;      //write f at last for float double and long
+   		System.out.println(value2);
+   		
+   		
+   		double value3=100d/3d;
+   		System.out.println(value3);
+   		
+   		
+   		int radius=10;
+   		double area =3.14*radius*radius;
+   		System.out.println(area);
+   	
+   	//char
+   		char ch='a';//use only single inverted comma
+   		System.out.println(ch);
 	}
 
 }
