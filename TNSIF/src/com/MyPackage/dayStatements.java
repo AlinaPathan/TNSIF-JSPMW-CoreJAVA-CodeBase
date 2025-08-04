982b7e7cf09ef5e1374bb0f9e808a50368f3dcb8
HP VICTUS <HP VICTUS@DESKTOP-U3RLE1B>
2025-08-04 18:20:10 +0530

    stsements done

--- TNSIF/src/com/MyPackage/dayStatements.java
+++ TNSIF/src/com/MyPackage/dayStatements.java
@@ -9,8 +9,6 @@ public class dayStatements {
 			System.out.println("Sorry!!! you cant vote");
 		}
 
-		
-		
 		int a=10,b=12,c=14;
 		if(a>b) {
 			if(a>c) {
@@ -23,6 +21,35 @@ public class dayStatements {
 				System.out.println("b is grater");
 			}else {
 				System.out.println("c is greater");
+			}
+		}
+		
+		//nested if else
+		int pass=12345;
+		String email="alinapathan@gmail.com";
+		
+		if(pass==12345) {
+			
+			if(email=="alinapathan@gmail.com") {
+				
+				System.out.println("email and password both are correct");
+				
+			}else {
+				
+				System.out.println("Password is correct but email is wrong");
+				
+			}
+			
+		}else {
+			
+			if(email=="alinapathan@gmail.com") {
+				
+				System.out.println("email is correct and password is wrong");
+				
+			}else {
+		
+					System.out.println("email and password both are wrong");
+				
 			}
 		}
  }
