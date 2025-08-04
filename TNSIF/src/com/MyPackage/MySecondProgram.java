998dda0dfbcd6a955e122c1c29d3fe5a35dfeaea
HP VICTUS <HP VICTUS@DESKTOP-U3RLE1B>
2025-08-04 12:21:28 +0530

    anather program

--- TNSIF/src/com/MyPackage/MySecondProgram.java
+++ TNSIF/src/com/MyPackage/MySecondProgram.java
@@ -1,10 +1,38 @@
 package com.MyPackage;
 
+//type casting
+
+
 public class MySecondProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println();
+        //implicit-widening
+		
+		byte b=10;
+		int i=b;
+		
+		
+		float f=22.14f;
+		double d=f;
+		System.out.println(d);
+		
+		char c='A';
+		int a=c;
+		System.out.println(a);
+		
+		
+		//explicite type casting
+		
+		double f1=10.4245f;
+		long ll=(long)f1;
+		System.out.println(ll);
+		
+		int i2=130;
+		byte b1=(byte)i2;
+		System.out.println(b1);//loop over range of byte -128 to 127
+				
+		
 	}
 
 }
