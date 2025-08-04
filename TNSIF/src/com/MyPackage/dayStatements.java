96b1d26d047d048cb5f615496595e17a492d1c78
HP VICTUS <HP VICTUS@DESKTOP-U3RLE1B>
2025-08-04 12:30:23 +0530

    done

--- TNSIF/src/com/MyPackage/dayStatements.java
+++ TNSIF/src/com/MyPackage/dayStatements.java
@@ -1,12 +1,14 @@
 package com.MyPackage;
 
 public class dayStatements {
-	int age= 26;
-	if (age>=18) {
-		System.out.println("Congratulations!!! you can vote now");
-	}else {
-		System.out.println("Sorry!!! you cant vote");
-	}
+ public static void main(String[] args) {
+		int age= 26;
+		if (age>=18) {
+			System.out.println("Congratulations!!! you can vote now");
+		}else {
+			System.out.println("Sorry!!! you cant vote");
+		}
 
+ }
 
 }
