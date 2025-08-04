5b15599077cf2a2dbb4b29048a6661cc8525c2d4
HP VICTUS <HP VICTUS@DESKTOP-U3RLE1B>
2025-08-04 12:25:32 +0530

    vote program

---
+++ TNSIF/src/com/MyPackage/day4Statements.java
@@ -0,0 +1,12 @@
+package com.MyPackage;
+
+public class day4Statements {
+	int age= 26;
+	if (age>=18) {
+		System.out.println("Congratulations!!! you can vote now");
+	}else {
+		System.out.println("Sorry!!! you cant vote");
+	}
+
+
+}
