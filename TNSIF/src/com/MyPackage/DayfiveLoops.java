f0eded5e36ccd2df24a82c839ba2a7f65b326be2
HP VICTUS <HP VICTUS@DESKTOP-U3RLE1B>
2025-08-05 20:27:09 +0530

    loops done

--- TNSIF/src/com/MyPackage/DayfiveLoops.java
+++ TNSIF/src/com/MyPackage/DayfiveLoops.java
@@ -3,53 +3,53 @@ package com.MyPackage;
 public class DayfiveLoops {
 	
 	public static void main(String[] args) {
-//	//loops 
-//	//1) for loop
-//	int arr[]= {1,3,5,7,8};
-//	int n=arr.length;
-//	for (int i=0;i<n;i++) {
-//		System.out.print(arr[i]+ " ");
-//	}
-//	System.out.println();
-//	
-//	
-//	//while loop
-//	
-//	int userInput=95;
-//	while(userInput<=100) {
-//		System.out.print(userInput+ " ");
-//		userInput++;
-//	}
-//System.out.println();
-//	
-//	
-//	
-//     //do while loop
-//	int i=11;
-//	do {
-//		System.out.print(i);
-//		i++;
-//	}while(i<10);
-//	
-//	System.out.println();
-//	
-//	
-//	
-//	//for each loop onlu for arrays or collections
-//	int arr1[]= {1,4,7,3,9,4,7,};
-//	for (int j:arr1) {
-//		System.out.print(j+ " ");
-//	}
-//	
-//	//nested for loop
-//	//print table from 10 to 20 
-//	for (int i=10;i<=20;i++) {
-//		for(int j=1;j<=10;j++) {
-//			System.out.print(i*j+ "   ");
-//		}
-//		System.out.println();
-//	}
-//	
+	//loops 
+	//1) for loop
+	int arr[]= {1,3,5,7,8};
+	int n=arr.length;
+	for (int i=0;i<n;i++) {
+		System.out.print(arr[i]+ " ");
+	}
+	System.out.println();
+	
+	
+	//while loop
+	
+	int userInput=95;
+	while(userInput<=100) {
+		System.out.print(userInput+ " ");
+		userInput++;
+	}
+System.out.println();
+	
+	
+	
+     //do while loop
+	int i=11;
+	do {
+		System.out.print(i);
+		i++;
+	}while(i<10);
+	
+	System.out.println();
+	
+	
+	
+	//for each loop onlu for arrays or collections
+	int arr1[]= {1,4,7,3,9,4,7,};
+	for (int j:arr1) {
+		System.out.print(j+ " ");
+	}
+	
+	//nested for loop
+	//print table from 10 to 20 
+	for (int i=10;i<=20;i++) {
+		for(int j=1;j<=10;j++) {
+			System.out.print(i*j+ "   ");
+		}
+		System.out.println();
+	}
+	
 		//pattern
 		for(int i=0;i<5;i++) {
 			for (int j=0;j<5;j++) {
