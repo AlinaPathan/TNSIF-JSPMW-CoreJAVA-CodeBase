b889dbf8ab56775da82fafb4f9813ee03bcf664f
HP VICTUS <HP VICTUS@DESKTOP-U3RLE1B>
2025-08-05 20:26:20 +0530

    staements done

--- TNSIF/src/com/MyPackage/JumpStaements.java
+++ TNSIF/src/com/MyPackage/JumpStaements.java
@@ -7,7 +7,47 @@ public class JumpStaements {
 		//jump statements
 		
 		
-
+		//break -execution is stopped when we use break
+		for (int i=5;i<10;i++) {
+			if(i==5) 
+				break;
+				System.out.println(i);
+				System.out.println("Hello Freinds");
+				//break;
+			
+		}
+		
+		
+		
+		//continue statements-
+		for (int k=5;k<15;k++) {
+			//skip odd numbers
+			if(k%2 !=0 ) {
+				continue;
+			}
+			System.out.print(k+ " ");
+		}
+System.out.println();
+		
+		
+		
+		//switch statements
+		//example of customer care
+		int userInput=8;
+		switch(userInput) {
+		case 1:System.out.println(userInput+ ": current plan details");
+		break;
+		case 2:System.out.println(userInput+ ": update plan");
+		break;
+		case 3:System.out.println(userInput+ ": customer support");
+		break;
+		case 4:System.out.println(userInput+ ": new plan overviews");
+		break;
+		default:
+			System.out.println("repeat Everything");
+		}
+		
+		
 	}
 
 }
